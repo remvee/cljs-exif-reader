e39a6dd204442b6708ce5f7d4c1b5963b73e29c3
Remco van 't Veer <remco@remworks.net>
2015-08-26 16:18:58 +0200

    Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,12 +1,17 @@
-(defproject remworks/cljs-exif-reader "0.1.0-SNAPSHOT"
+(defproject remworks/cljs-exif-reader "0.1.0"
   :description "Extract information from TIFF and JPEG images."
+
+  :url "https://github.com/remvee/cljs-exif-reader"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :clojurescript? true
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3126"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.48"]]
 
-  :plugins [[lein-cljsbuild "1.0.5"]]
+  :plugins [[lein-cljsbuild "1.1.0"]]
 
   :cljsbuild {:builds {:dev {:source-paths ["src" "dev"]
-                             :compiler {:output-to "target/cljs/exif-reader.js"}}}})
+                             :compiler {:output-dir "target/cljs"
+                                        :output-to "target/cljs/exif-reader.js"}}}})

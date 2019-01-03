2319d07aa34a641dfca5d74fc22da214f066185d
Remco van 't Veer <remco@remworks.net>
2019-01-03 09:55:25 +0100

    Release 0.2.0

--- project.clj
+++ project.clj
@@ -1,25 +1,30 @@
-(defproject remworks/cljs-exif-reader "0.2.0-SNAPSHOT"
+(defproject remworks/cljs-exif-reader "0.2.0"
   :description "Extract information from TIFF and JPEG images."
 
   :url "https://github.com/remvee/cljs-exif-reader"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :clojurescript? true
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.48"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.439"]]
 
-  :plugins [[lein-cljsbuild "1.1.0"]
-            [lein-doo "0.1.4"]]
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-doo "0.1.10"]]
 
-  :cljsbuild {:builds {:dev {:source-paths ["src" "dev"]
-                             :compiler {:output-dir "target/cljs"
-                                        :output-to "target/cljs/exif-reader.js"}}
+  :doo {:build "test"
+        :alias {:default [:node]}}
+
+  :aliases {"test" ["doo" "node" "test" "once"]}
+
+  :cljsbuild {:builds {:dev  {:source-paths ["src" "dev"]
+                              :compiler     {:output-dir "target/cljs"
+                                             :output-to  "target/cljs/exif-reader.js"}}
                        :test {:source-paths ["src" "test"]
-                              :compiler {:optimizations :none
-                                         :main 'remworks.runner
-                                         :output-dir "target/test"
-                                         :output-to "target/test/exif-reader-test.js"
-                                         :target :nodejs
-                                         :hashbang false}}}})
+                              :compiler     {:optimizations :none
+                                             :output-dir    "target/test"
+                                             :output-to     "target/test/exif-reader-test.js"
+                                             :target        :nodejs
+                                             :main          remworks.runner
+                                             :hashbang      false}}}})
